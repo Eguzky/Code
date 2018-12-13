eec44c5f88e30272803db22e5308d10f1f421b0e
Koden02 <koden_kabau@yahoo.com>
2018-12-13 02:04:57 -0500

    Update KAI.py
    added two levels of intelligence, also made it that it will no longer do the same guess twice.

--- KAI.py
+++ KAI.py
@@ -6,31 +6,46 @@ class AI:
             self._outer = outer
             self.havesets = {}
 
-    def __init__(self, playerfile : Deck.Hand):
+    def __init__(self, playerfile : Deck.Hand, smarts : int = 0):
         self.playerfile = playerfile
         self.memory = self.Memory(self)
+        self.smarts = smarts
         
 
 
 
 class AI_Gofish(AI):
+
     def setscheck(self):
         self.memory.setscheck()
     
     def guesscard(self) -> str:
         self.setscheck()
         hand = self.memory.havesets
-
-        sort = sorted(hand.items(), key= lambda kv: kv[0])
-        highval = hand[sort[0][0]]
-        
-        choices = []
-        for key in hand:
-            if hand[key] == highval:
+        selected = ""
+        if len(hand) > 1 and self.memory.lastcall != "" and self.memory.lastcall in hand:
+            hand.pop(self.memory.lastcall)
+        if self.smarts == 0:
+            sort = sorted(hand.items(), key= lambda kv: kv[0])
+            highval = hand[sort[0][0]]
+            
+            choices = []
+            for key in hand:
+                if hand[key] == highval:
+                    choices.append(key)
+            selected = random.choice(choices)
+        else:
+            choices = []
+            strengths = []
+            for key in hand:
+                # if hand[key] == highval:
                 choices.append(key)
-        selected = random.choice(choices)
+                strengths.append(hand[key] * 25)
+            selected = random.choices(choices, strengths, k=1)[0]
+        self.memory.lastcall = selected
         if selected == "T":
             selected = "10"
+        
         return selected
 
 
@@ -45,6 +60,7 @@ class AI_Gofish(AI):
         def __init__(self, outer):
             super().__init__(outer)
             self.setscheck()
+            self.lastcall = ""
         
 
 
