1274c8d78539e8e0d88df9fb1dad158243f059dd
Eguzky <eguzky84@gmail.com>
2018-12-16 23:38:04 -0500

    Update NameGenerator.py

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,17 +1,21 @@
-import NameLists
 import random
+from NameLists import male, female, last
 
+def male_namegen():
+    male_group = []
+    for x in range(1):
+        male_name = random.choice(male).capitalize()+" "+random.choice(last).capitalize()
+        male_group.append(male_name)
+    print('Male Names: ')
+    print("\n".join(male_group))
 
-male_group = []
-for x in range(6):
-    from NameLists import male, last
-    male_name = random.choice(male)+"  "+random.choice(last)
-    male_group.append(male_name)
-print(", ".join(male_group))
+def female_namegen():
+    female_group = []
+    for x in range(1):
+        female_name = random.choice(female).capitalize()+" "+random.choice(last).capitalize()
+        female_group.append(female_name)
+    print('Female Names:')
+    print("\n".join(female_group))
 
-female_group = []
-for x in range(6):
-    from NameLists import female, last
-    female_name = random.choice(female)+"  "+random.choice(last)
-    female_group.append(female_name)
-print(", ".join(female_group))
+def namegen():
+    input('[M]ale or [F]emale?')
